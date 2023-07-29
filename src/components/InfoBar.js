0df3f8afccc43cc0b4614a33a7a492ce02896e9b
BuildTools <unconfigured@null.spigotmc.org>
2023-07-30 01:31:54 +0800

    Added inactive name color

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -5,7 +5,7 @@ function InfoBar({facing}){
   const { currentPlayer, players } = useGameStore();
   return(
     <div className="flex min-w-[30vmin] border-[0.3vmin] h-[10vmin]">
-      <div className="bg-gray-300 flex-grow mr-[2vmin]">{players[facing].nickname} {currentPlayer.facing === facing ?'<<':''}</div>
+      <div className={"bg-gray-300 flex-grow mr-[2vmin]" + (players[facing].inactive ? ' opacity-60' : '')}>{players[facing].nickname} {currentPlayer.facing === facing ?'<<':''}</div>
       <div className="bg-green-300 mr-[2vmin]">179:59</div>
       <PieceStand facing={facing}/>
     </div>
